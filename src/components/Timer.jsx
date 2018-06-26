a676fdae996c0702b5630a1b004828167c0954b3
Witek85 <witold.szklarski@gmail.com>
2018-06-26 12:22:13 +0200

    allowmultiple

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -5,11 +5,11 @@ import CSSModules from 'react-css-modules';
 
 
 const Timer = () => (
-    <div styleName="border" className="global">
+    <div styleName="border grey" className="global">
         <span>13</span>:
         <span>21</span>:
         <span>12</span>
     </div>
 )
 
-export default CSSModules(Timer, style);
+export default CSSModules(Timer, style, {allowMultiple: true});
