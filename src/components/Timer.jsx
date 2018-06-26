beab1c6905e0676010bc032a85bfe9758e2b96c2
Witek85 <witold.szklarski@gmail.com>
2018-06-26 12:20:36 +0200

    prawdziwe css modules

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import style from './Timer.scss';
+import CSSModules from 'react-css-modules';
+
 
 const Timer = () => (
-    <div className={style.border}>
+    <div styleName="border" className="global">
         <span>13</span>:
         <span>21</span>:
         <span>12</span>
     </div>
 )
 
-export default Timer;
+export default CSSModules(Timer, style);
